683d6cbd487bb7fe6719f8cd3e22d903c113161b
Rachit Shrivastava <funcococode@github.com>
2025-08-26 16:46:56 +0530

    changed readme and favicons

--- app/root.tsx
+++ app/root.tsx
@@ -21,6 +21,10 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  { rel: "icon", href: "/favicon.ico" },
+  { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+  { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+  { rel: "apple-touch-icon", sizes: "180x180", href: "/sjtravels-apple-touch-icon-180.png" },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
